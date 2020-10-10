3d928f01185b8d79aefae99494ab87ea4b4a083a
zangchao <zangc@koal.com>
2020-10-10 18:20:16 +0800

    update

--- insert_db.py
+++ insert_db.py
@@ -9,10 +9,11 @@ def get_timestamp(get_time):
 conn = sqlite3.connect("tia.db")
 c = conn.cursor()
 
-# timestamp = get_timestamp("2020-09-18 00:00:00")
+# timestamp = get_timestamp("2020-09-16 20:00:00")
 # print(timestamp)
-str_sql = "INSERT INTO Tia VALUES (1596988800, '2020-08-10 00:00:00 107 107 test 打开 中阶初级技能 获得:初级吞噬技能书');"
-# str_sql = "INSERT INTO Tia VALUES (1600358400, '2020-09-18 00:00:00 107 107 test 打开 中阶初级技能 获得:初级暗影之舞技能书');"
+# str_sql = "INSERT INTO Tia VALUES (1600257600, '2020-09-16 20:00:00 107 107 test 打开 三阶专属装备 获得:符纹靴');"
+str_sql = "INSERT INTO Tia VALUES (1596988800, '2020-08-10 00:00:00 111 111 test 打开 三阶专属装备 获得:魔法棍');"
+# str_sql = "INSERT INTO Tia VALUES (1600948800, '2020-09-24 20:00:00 107 107 test 打开 高阶初级技能 获得:初级治疗光波技能书');"
 c.execute(str_sql)
 conn.commit()
 conn.close()
--- tia_data.py
+++ tia_data.py
@@ -42,7 +42,7 @@ lieren_book_m     = ["变熊","野性激发","剧毒标枪","绝望之囚","自�
 # 嗜血？割喉？突袭？窒息之刃？铁钩？真空=反向?进攻光环?地震
 wushi_book_h      = ["闪电护盾","穿刺风暴","割裂","幻影斩击","遁地突刺","割喉"]
 zhanshi_book_h    = ["挥击","神的力量","烧灼","毁灭","岩石皮肤","地震"]
-qishi_book_h      = ["圣光守护","惩戒","钢铁意志","圣光驱散","神盾","进攻光环"]
+qishi_book_h      = ["圣光守护","惩戒","刚铁意志","圣光驱散","神盾","进攻光环"]
 fashi_book_h      = ["暴风雪","寒冰咆哮","寒冬","流星火雨","真空","雷神之力"]
 saman_book_h      = ["崩溃","炎魔","虚弱","死亡生长","陨石","嗜血"]
 mushi_book_h      = ["守护天使","寂静守卫","烈炎重生","自然守护","治疗光波","净化术"]
@@ -53,3 +53,30 @@ lieren_book_h     = ["寒冰标枪","白熊","狮吼","铁钩","掩护","变豹"
 zhiye_def_l = [wushi_book_l, zhanshi_book_l, qishi_book_l, fashi_book_l, saman_book_l, mushi_book_l, qiang_book_l, lieren_book_l, cike_book_l]
 zhiye_def_m = [wushi_book_m, zhanshi_book_m, qishi_book_m, fashi_book_m, saman_book_m, mushi_book_m, qiang_book_m, lieren_book_m, cike_book_m]
 zhiye_def_h = [wushi_book_h, zhanshi_book_h, qishi_book_h, fashi_book_h, saman_book_h, mushi_book_h, qiang_book_h, lieren_book_h, cike_book_h]
+
+jianshi_tt      = ["推进盔", "推进铠", "推进盾", "推进手套", "推进护靴", "勇士剑"]
+mofa_tt         = ["灵动帽", "灵动法袍", "灵动之书", "灵动护手", "灵动鞋", "灵动杖"]
+sheshou_tt      = ["防卫皮帽", "复合甲", "防卫剑", "防卫手套", "防卫鞋", "复合弓"]
+tt_za           = ["金项链", "白银腰带", "精皮水袋"]
+
+tt_zhuang_def   = [jianshi_tt, mofa_tt, sheshou_tt]
+
+wushi_tz        = ["嵌甲盔", "镶嵌甲", "覆铁盾", "嵌甲护胫", "嵌甲护手", "冷刃矛"]
+zhanshi_tz      = ["重型盔", "巨型甲", "重型盾", "重型护胫", "重型手套", "巨型斧"]
+qishi_tz        = ["符纹盔", "符纹甲", "符纹盾", "符纹靴", "符纹护手", "符纹权杖"]
+fashi_tz        = ["魔法帽", "魔法服", "魔法大典", "魔法鞋", "魔法手套", "魔法棍"]
+saman_tz        = ["黑暗面具", "黑暗之衣", "黑暗之书", "黑暗鞋", "黑暗护腕", "羊头杖"]
+mushi_tz        = ["祈祷帽", "祈祷服", "祈祷经文", "祈祷鞋", "祈祷手套", "祈祷杖"]
+qiang_tz        = ["毡帽", "层压甲", "花剑", "军用靴", "军用护手", "膛线枪"]
+lieren_tz       = ["重皮帽", "重皮甲", "锐利猎刀", "重皮靴", "重皮手套", "重标枪"]
+cike_tz         = ["硬皮面罩", "硬皮甲", "锋利匕首", "硬皮护胫", "硬皮束手", "三角镖"]
+tz_za           = ["秘银水壶", "秘银束带", "魔金项链"]
+
+tz_zhuang_def = [wushi_tz, zhanshi_tz, qishi_tz, fashi_tz, saman_tz, mushi_tz, qiang_tz, lieren_tz, cike_tz]
+
+jianshi_ft      = ["坚固盔", "坚固铠", "坚固盾", "坚固护腕", "坚固护靴", "斩铁剑"]
+mofa_ft         = ["诡秘之帽", "诡秘法袍", "诡秘之书", "诡秘手套", "诡秘之鞋", "诡秘杖"]
+sheshou_ft      = ["圆月之帽", "圆月甲", "寒铁剑", "圆月手套", "圆月鞋", "弯月弓"]
+ft_za           = ["水晶吊坠", "镶金腰带", "密纹水袋"]
+
+ft_zhuang_def   = [jianshi_ft, mofa_ft, sheshou_ft]
