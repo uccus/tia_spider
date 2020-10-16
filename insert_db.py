df27fa2faf3073a50726066a1b3dd053cb174fd5
zangchao <zangc@koal.com>
2020-10-16 20:03:29 +0800

    update

--- insert_db.py
+++ insert_db.py
@@ -12,7 +12,7 @@ c = conn.cursor()
 # timestamp = get_timestamp("2020-09-16 20:00:00")
 # print(timestamp)
 # str_sql = "INSERT INTO Tia VALUES (1600257600, '2020-09-16 20:00:00 107 107 test 打开 三阶专属装备 获得:符纹靴');"
-str_sql = "INSERT INTO Tia VALUES (1596988800, '2020-08-10 00:00:00 111 111 test 打开 三阶专属装备 获得:魔法棍');"
+str_sql = "INSERT INTO Tia VALUES (1596988800, '2020-08-10 00:00:00 107 107 test 打开 高阶初级技能 获得:初级割裂技能书');"
 # str_sql = "INSERT INTO Tia VALUES (1600948800, '2020-09-24 20:00:00 107 107 test 打开 高阶初级技能 获得:初级治疗光波技能书');"
 c.execute(str_sql)
 conn.commit()
